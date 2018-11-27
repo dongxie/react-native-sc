1d652281e2c8aa8d58b92685d4f180c03177758b
dongxie <dongxie@dongxie.cn>
2018-11-27 15:20:21 +0800

    no message

--- ios/sc/sc.m
+++ ios/sc/sc.m
@@ -1,15 +1,15 @@
 //
-//  PTRIDFA.m
-//  IDFA
+//  sc.m
+//  SC
 //
-//  Created by Tomas Roos on 22/07/16.
-//  Copyright © 2016 Tomas Roos. All rights reserved.
 //
 
 #import "sc.h"
 #import <React/RCTUtils.h>
 #import <UIKit/UIKit.h>
 #import "OpenInstallSDK.h"
+#import "TalkingData.h"
+#import "hsjson.h"
 @import AdSupport;
 
 @implementation sc
@@ -38,13 +38,43 @@ RCT_EXPORT_METHOD(getOpenInstallData:(RCTPromiseResolveBlock)resolve
     }];
     
 }
-
-RCT_EXPORT_METHOD(openInstallReportRegister){
+//{uid...nickname....}
+RCT_EXPORT_METHOD(ReportRegister:(NSString*)data){
+    NSMutableDictionary *body=[NSJSONSerialization objectWithString:data];
     //用户注册成功后调用
     [OpenInstallSDK reportRegister];
+    [TalkingData onRegister:[[body objectForKey:@"uid"] stringValue] type:TDAccountTypeRegistered name:[body objectForKey:@"nickname"]];
 }
-RCT_EXPORT_METHOD(openInstallReportEffectPoint:(NSString *)pointId
-                                          with:(NSNumber *)point){
-    [[OpenInstallSDK defaultManager] reportEffectPoint:pointId effectValue:[point integerValue]];//value为整型,如果是人民币金额，请以分为计
+RCT_EXPORT_METHOD(ReportLogin:(NSString*)data){
+    NSMutableDictionary *body=[NSJSONSerialization objectWithString:data];
+    [TalkingData onRegister:[[body objectForKey:@"uid"] stringValue] type:TDAccountTypeRegistered name:[body objectForKey:@"nickname"]];
+}
+//talkingData {event:"事件id",label:"事件名字",defaultValue:"默认值，比如第一个kv" ,param:{k:v...}}
+RCT_EXPORT_METHOD(ReportEffectPoint:(NSString*)data){
+    NSMutableDictionary *body=[NSJSONSerialization objectWithString:data];
+    //value为整型,如果是人民币金额，请以分为计
+    [[OpenInstallSDK defaultManager] reportEffectPoint:[[body objectForKey:@"event"] stringValue] effectValue:[[body objectForKey:@"defaultValue"] integerValue]];
+    [TalkingData trackEvent: [body objectForKey:@"event"] label: [body objectForKey:@"label"] parameters: [body objectForKey:@"param"]];
+}
+/**
+ *  @method    trackPageBegin
+ *  开始跟踪某一页面（可选），记录页面打开时间
+ 建议在viewWillAppear或者viewDidAppear方法里调用
+ *  @param  pageName    页面名称（自定义）
+ */
+RCT_EXPORT_METHOD(trackPageBegin:(NSString *)pageName){
+    [TalkingData trackPageBegin:pageName];
+}
+
+/**
+ *  @method trackPageEnd
+ *  结束某一页面的跟踪（可选），记录页面的关闭时间
+ 此方法与trackPageBegin方法结对使用，
+ 在iOS应用中建议在viewWillDisappear或者viewDidDisappear方法里调用
+ 在Watch应用中建议在DidDeactivate方法里调用
+ *  @param  pageName    页面名称，请跟trackPageBegin方法的页面名称保持一致
+ */
+RCT_EXPORT_METHOD(trackPageEnd:(NSString *)pageName){
+    [TalkingData trackPageEnd:pageName];
 }
 @end
--- sc.js
+++ sc.js
@@ -1,34 +1,94 @@
 import { NativeModules } from 'react-native';
 
-const { PTRIDFA } = NativeModules;
+const { sc } = NativeModules;
 
-class IDFA {
-  getIDFA() {
-    return PTRIDFA.getIDFA();
-  }
-
-  getOpenInstallData() {
-    if(!PTRIDFA.hasOwnProperty("getOpenInstallData")){
-      return new Promise( ( resolve, reject ) =>{
-          resolve({})
-      });
+class SC {
+    getIDFA() {
+        return PTRIDFA.getIDFA();
     }
-    return PTRIDFA.getOpenInstallData();
-  }
 
-  openInstallReportRegister() {
-      if(!PTRIDFA.hasOwnProperty("openInstallReportRegister")){
-          return;
-      }
-      PTRIDFA.openInstallReportRegister();
-  }
-  openInstallReportEffectPoint(pointId,point) {
-      if(!PTRIDFA.hasOwnProperty("openInstallReportEffectPoint")){
-          return;
-      }
-      PTRIDFA.openInstallReportEffectPoint(pointId,point);
-  }
+    getOpenInstallData() {
+        if(!sc.hasOwnProperty("getOpenInstallData")){
+            return new Promise( ( resolve, reject ) =>{
+                resolve({})
+            });
+        }
+        return sc.getOpenInstallData();
+    }
+
+    /**
+     * 注册时发送
+     * @param uid
+     * @param nickname
+     * @constructor
+     */
+    ReportRegister(uid,nickname) {
+        if(!sc.hasOwnProperty("ReportRegister")){
+            return;
+        }
+
+        sc.ReportRegister(JSON.stringify({uid,nickname}));
+    }
+
+    /**
+     * 登录打点
+     * @param uid
+     * @param nickname
+     * @constructor
+     */
+    ReportLogin(uid,nickname) {
+        if(!sc.hasOwnProperty("ReportLogin")){
+            return;
+        }
+        sc.ReportLogin(JSON.stringify({uid,nickname}));
+    }
+
+    /**
+     * 开始跟踪某一页面（可选），记录页面打开时间
+     * @param pageName
+     */
+    trackPageBegin(pageName) {
+        if(!sc.hasOwnProperty("trackPageBegin")){
+            return;
+        }
+        sc.trackPageBegin(pageName);
+    }
+
+    /**
+     * 结束某一页面的跟踪（可选），记录页面的关闭时间
+     * @param pageName
+     */
+    trackPageEnd(pageName) {
+        if(!sc.hasOwnProperty("trackPageBegin")){
+            return;
+        }
+        sc.trackPageEnd(pageName);
+    }
+
+    /**
+     *跟踪事件
+     * @param event
+     * @param label 事件标签或事件数值
+     * @param param
+     * @constructor
+     */
+    ReportEffectPoint(event,label,param) {
+        if(!sc.hasOwnProperty("ReportEffectPoint")){
+            return;
+        }
+        let defaultValue=0;
+        if(!label&&label!==0){
+            label="";
+        }else if(!isNaN(label)){
+            defaultValue=label;
+            label=label+"";
+        }else{
+            defaultValue=0;
+        }
+        if(!param)param={};
+        sc.ReportEffectPoint({event,label,defaultValue,param});
+    }
 }
 
-IDFAInstance = new IDFA();
-export default IDFAInstance;
+SCInstance = new SC();
+export default SCInstance;
